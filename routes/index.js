e33bd891726a338eee9b38519fa5acad9795b6f0
h4tl2 <hat9458g@gmail.com>
2018-06-16 17:36:12 +0700

    camp endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const express = require('express')
 const router = express.Router()
 
 const test = require('./routeTest');
+const camp = require('./routeCamp');
 
 router.use('/test', test);
+router.use('/camp', camp);
 
 module.exports = router;
