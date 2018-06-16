beea464ef0272501609df1bd4ced714a60f2e364
h4tl2 <hat9458g@gmail.com>
2018-06-16 18:29:41 +0700

    -

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,7 @@ const express = require('express')
 const router = express.Router()
 
 const test = require('./routeTest');
-const camp = require('./routeCamp');
 
 router.use('/test', test);
-router.use('/camp', camp);
 
 module.exports = router;
