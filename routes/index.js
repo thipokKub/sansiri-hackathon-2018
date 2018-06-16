577f7431cb52502a667825af5f511b06eba40c49
h4tl2 <hat9458g@gmail.com>
2018-06-16 19:19:50 +0700

    worker

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,12 @@ const router = express.Router()
 const test = require('./routeTest');
 const camp = require('./routeCamp');
 const supplier = require('./routeSupplier');
+const worker = require('./routeWorker');
 
 
 router.use('/test', test);
 router.use('/camp', camp);
 router.use('/supplier',supplier);
+router.use('/worker',worker);
 
 module.exports = router;
